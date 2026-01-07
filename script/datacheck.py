2dd3e392cd1b45e9eb5d3ab822b3a1800e4ca873
liubquanti <doroleijn@gmail.com>
2026-01-07 13:16:00 +0100

    Datacheck script

---
+++ script/datacheck.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+csv_file = r'data\ADECal.csv'
+
+data = pd.read_csv(csv_file)
+
+unique_groups = data['Summary'].unique()
+unique_groups = sorted(map(str, unique_groups))
+
+for group in unique_groups:
+    print(group)
